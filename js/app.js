b78c7b36fde42d54babc891ebf462cdf990b161c
waldev <waldoaraque10@gmail.com>
2020-04-04 17:15:51 +0200

    using a json file to test fetch() method

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 document.getElementById('txtBtn').addEventListener('click', loadTxt)
 
+document.getElementById('jsonBtn').addEventListener('click', loadJSON)
 function loadTxt() {
     fetch('datos.txt')
         .then( res => {
@@ -9,7 +10,29 @@ function loadTxt() {
             console.log(data)
             document.getElementById('resultado').innerHTML = data
         })
-        .catch( e => {
-            console.log(e)
+        .catch( err => {
+            console.log(err)
+            throw new Error(err)
+        })
+}
+
+function loadJSON() {
+    fetch('empleados.json')
+        .then( res => {
+            return res.json()
+        })
+        .then( data => {
+            console.log(data)
+            let html = ''
+            data.forEach( d => {
+                html += `
+                    <li>${d.nombre} ${d.puesto}</li>
+                `
+            })
+            document.getElementById('resultado').innerHTML = html
+        })
+        .catch( err => {
+            console.log(err)
+            throw new Error(err)
         })
 }
